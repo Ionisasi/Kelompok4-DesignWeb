5ce846fb168c07bdaec68e71f0ca29bbb8e66d33
Febryanda <139682985+Ionisasi@users.noreply.github.com>
2025-07-02 19:44:43 +0700

    minor responsive update

--- Script/navbar.js
+++ Script/navbar.js
@@ -1,14 +1,15 @@
-// navbar.js
+const currentPage = location.pathname.split('/').pop();
+
 document.write(`
   <div class="nav-bar">
     <img class="main-logo" src="Asset/main-logo0.png" alt="Logo" />
     <div class="frame-7"></div>
     <menu class="nav-menu">
-      <a href="halamanUtama.html" class="nav-link active">Beranda</a>
-      <a href="edukasi.html" class="nav-link">Edukasi</a>
-      <a href="kisahInspirasiIrma.html" class="nav-link">Kisah Inspirasi</a>
-      <a href="about.html" class="nav-link">Tentang Kami</a>
-      <a href="kontak.html" class="nav-link">Kontak</a>
+      <a href="halamanUtama.html" class="nav-link ${currentPage === 'halamanUtama.html' ? 'active' : ''}">Beranda</a>
+      <a href="edukasi.html" class="nav-link ${currentPage === 'edukasi.html' ? 'active' : ''}">Edukasi</a>
+      <a href="kisahInspirasiIrma.html" class="nav-link ${currentPage === 'kisahInspirasiIrma.html' ? 'active' : ''}">Kisah Inspirasi</a>
+      <a href="about.html" class="nav-link ${currentPage === 'about.html' ? 'active' : ''}">Tentang Kami</a>
+      <a href="kontak.html" class="nav-link ${currentPage === 'kontak.html' ? 'active' : ''}">Kontak</a>
     </menu>
     <button class="burger-btn">
       <span class="burger-line"></span>
